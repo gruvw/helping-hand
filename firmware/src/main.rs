3de96c2d11eb5ea50e8a1c7726110467d96c8171
gruvw <gruvw.dev@gmail.com>
2026-03-20 19:33:38 +0100

    feat: servo control and sweep

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,50 +1,91 @@
-use anyhow::Result;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::units::FromValueType;
 use pwm_pca9685::{Address, Channel, Pca9685};
 
-fn main() -> Result<()> {
+macro_rules! debug_println {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*)
+    };
+}
+
+// properties of the servo
+const SERVO_FREQUENCY: u32 = 50; // 50 Hz
+const SERVO_ANGLE_MIN: f32 = 0.0;
+const SERVO_ANGLE_MAX: f32 = 180.0;
+const SERVO_ANGLE_MIN_TIME_MS: f32 = 0.5;
+const SERVO_ANGLE_MAX_TIME_MS: f32 = 2.5;
+
+// properties of the PCA9685
+const PCA9685_FREQUENCY: u32 = 25_000_000; // 25 MHz
+const PCA9685_RANGE: u32 = 4096; // 12 bits
+const I2C_BAUD_RATE: u32 = 100_000; // 100kHz
+
+// derived constants
+const SERVO_PERIOD_MS: f32 = 1000.0 / SERVO_FREQUENCY as f32;
+const SERVO_ANGLE_MIN_TICKS: u16 =
+    (SERVO_ANGLE_MIN_TIME_MS / SERVO_PERIOD_MS * PCA9685_RANGE as f32).round() as u16;
+const SERVO_ANGLE_MAX_TICKS: u16 =
+    (SERVO_ANGLE_MAX_TIME_MS / SERVO_PERIOD_MS * PCA9685_RANGE as f32).round() as u16;
+
+// prescale formula (PCA9685 datasheet 7.3.5)
+// prescale = round(osc_clock / (4096 × update_rate)) − 1
+const PCA9685_PRESCALE: u8 =
+    (PCA9685_FREQUENCY as f32 / (PCA9685_RANGE * SERVO_FREQUENCY) as f32 - 1.0).round() as u8;
+
+/// Set angle for the given `channel`, angle is clamped between `SERVO_ANGLE_MIN` and
+/// `SERVO_ANGLE_MAX`.
+fn move_to_angle<I2C>(pca: &mut Pca9685<I2C>, channel: Channel, angle: f32)
+where
+    I2C: embedded_hal::i2c::I2c,
+{
+    let angle = angle.clamp(SERVO_ANGLE_MIN, SERVO_ANGLE_MAX);
+
+    // linear interpolation between min and and max angles ticks
+    let ticks = (SERVO_ANGLE_MIN_TICKS as f32
+        + (angle - SERVO_ANGLE_MIN)
+            * ((SERVO_ANGLE_MAX_TICKS - SERVO_ANGLE_MIN_TICKS) as f32
+                / (SERVO_ANGLE_MAX - SERVO_ANGLE_MIN)))
+        .round() as u16;
+
+    debug_println!("PCA9685: set channel {:?} to {}°", channel, angle);
+
+    // signal goes high at time 0, goes low at time `ticks`
+    pca.set_channel_on_off(channel, 0, ticks)
+        .expect("failed to set PCA9685 channel on/off");
+}
+
+fn main() -> ! {
     esp_idf_svc::sys::link_patches();
 
-    let peripherals = Peripherals::take().unwrap();
+    debug_println!("device started");
+
+    let peripherals = Peripherals::take().expect("failed to take peripherals");
     let sda = peripherals.pins.gpio4;
     let scl = peripherals.pins.gpio5;
 
-    let config = I2cConfig::new().baudrate(100.kHz().into());
-    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config)?;
+    let config = I2cConfig::new().baudrate(I2C_BAUD_RATE.into());
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config)
+        .expect("failed to initialise I2C driver");
 
-    let mut pwm = Pca9685::new(i2c, Address::default()).unwrap();
+    let mut pca = Pca9685::new(i2c, Address::default()).expect("failed to initialise PCA9685");
 
-    // 50Hz frequency
-    pwm.set_prescale(121).unwrap();
-    pwm.enable().unwrap();
+    pca.set_prescale(PCA9685_PRESCALE)
+        .expect("failed to set PCA9685 prescale");
+    pca.enable().expect("failed to enable PCA9685");
 
-    // Corrected tick values for 0.5ms to 2.5ms range
-    let pos_0_degrees = 102; // 0.5ms
-    let pos_180_degrees = 512; // 2.5ms
-
-    println!("Starting snappy jumps...");
+    debug_println!("device intitialized");
 
     loop {
-        // 1. Jump to 180 degrees immediately
-        println!("Jumping to 180...");
-        pwm.set_channel_on_off(Channel::C0, 0, pos_180_degrees)
-            .unwrap();
-
-        // Wait 1 second for the physical arm to finish moving
+        move_to_angle(&mut pca, Channel::C0, SERVO_ANGLE_MAX);
         FreeRtos::delay_ms(1000);
 
-        // 2. Jump to 0 degrees immediately
-        println!("Jumping to 0...");
-        pwm.set_channel_on_off(Channel::C0, 0, pos_0_degrees)
-            .unwrap();
-
-        // Wait 1 second
+        move_to_angle(&mut pca, Channel::C0, SERVO_ANGLE_MIN);
         FreeRtos::delay_ms(1000);
     }
 }
+
 // use esp_idf_svc::eventloop::EspSystemEventLoop;
 // use esp_idf_svc::hal::peripherals::Peripherals;
 // use esp_idf_svc::http::server::Configuration;
